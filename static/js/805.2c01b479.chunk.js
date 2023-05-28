f0152794fbae4057c5bffc7cea0473340e4e2c40
SiciarzMat <61201511+SiciarzMat@users.noreply.github.com>
2023-05-28 16:21:39 +0000

    Deploying to gh-pages from @ SiciarzMat/goit-react-hw-08-phonebook@d016db70e39506177decca3de34ee4494ed9c4d0 🚀

---
+++ static/js/805.2c01b479.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[805],{9805:function(e,t,r){r.r(t);var c=r(2791),n=r(7689),u=r(184);t.default=function(){var e=(0,n.s0)();return(0,c.useEffect)((function(){e("/")})),(0,u.jsx)("div",{children:"404 :("})}}}]);
+//# sourceMappingURL=805.2c01b479.chunk.js.map
